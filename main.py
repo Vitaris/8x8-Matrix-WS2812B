07781584eb2aec908ce35d7d337c4464c753c2da
Vitaris <53018032+Vitaris@users.noreply.github.com>
2024-02-11 08:24:49 +0100

    Updated symbols

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ print(random.randint(0, 9))
 
 if __name__ == "__main__":
     matrix = matrix_8x8(28, 5, brightness=0.02)
-    matrix.show_symbol(smile, color=YELLOW)
-    matrix.show_symbol(circle, offset=1, color=RED)
-    matrix.show_symbol(circle, offset=2, color=ORANGE)
-    matrix.show_symbol(circle, offset=3, color=GREEN)
-    matrix.show_symbol(sad, offset=4, color=RED)
-
-    while False:
+    while True:
         matrix.show_number(random.randint(0, 99), color=random.choice(COLORS))
-        matrix.show_number(random.randint(0, 99), offset=1, color=random.choice(COLORS))
-        matrix.show_number(random.randint(0, 99), offset=2, color=random.choice(COLORS))
-        matrix.show_number(random.randint(0, 99), offset=3, color=random.choice(COLORS))
-        matrix.show_number(random.randint(0, 99), offset=4, color=random.choice(COLORS))
+        matrix.show_symbol(random.choice(SYMBOLS), offset=1, color=random.choice(COLORS))
+        matrix.show_symbol(random.choice(SYMBOLS), offset=2, color=random.choice(COLORS))
         utime.sleep(1)
     
     
